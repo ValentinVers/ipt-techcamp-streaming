907aa68bfc9a64f3c0ccfad63928866023c2e9e5
andrilareida <andrilareida@users.noreply.github.com>
2023-04-21 10:47:59 +0200

    update

--- data-producer/data-producer/data-producer/Program.cs
+++ data-producer/data-producer/data-producer/Program.cs
@@ -1,30 +1,22 @@
-﻿using Confluent.Kafka;
+﻿
+using Confluent.Kafka;
 using Azure.Core;
 using System;
 using Azure.Identity;
 
-
-//Start program
-KafkaProducer.Program.Main();
 
 namespace KafkaProducer
 {
 
     public class Program
     {
-        private static string eventHubNamespace = "alr-eventhub";
+        private static string eventHubNamespace = "techcamp";
+
 
         private static string eventHubNamespaceFQDNwithPort = $"{eventHubNamespace}.servicebus.windows.net:9093";
 
         //in kafka world this is the topic in event hub is the event hub name under the namespace
-        private static string topicName = "kafka-topic";
-
-        //Secret STuff
-        private static string tenantId = "";
-        private static string clientId = "";
-        private static string clientSecret = "";
-
-
+        private static string topicName = "test";
 
         static void OauthCallback(IClient client, string cfg)
         {
@@ -32,14 +24,14 @@ namespace KafkaProducer
             {
                 Console.WriteLine("Getting auth token");
 
-                ClientSecretCredentialOptions secretOptions = new ClientSecretCredentialOptions();
-                ClientSecretCredential secretCredential = new ClientSecretCredential(tenantId, clientId, clientSecret, secretOptions);
+                DefaultAzureCredentialOptions secretOptions = new DefaultAzureCredentialOptions();
+                DefaultAzureCredential secretCredential = new DefaultAzureCredential(secretOptions);
 
                 var tokenRequestContext = new TokenRequestContext(new string[] { $"https://{eventHubNamespace}.servicebus.windows.net/.default" });
 
                 var accessToken = secretCredential.GetToken(tokenRequestContext);
 
-                client.OAuthBearerSetToken(accessToken.Token, accessToken.ExpiresOn.Millisecond, null);
+                client.OAuthBearerSetToken(accessToken.Token, accessToken.ExpiresOn.ToUnixTimeMilliseconds(), null);
             }
             catch (Exception e)
             {
@@ -49,7 +41,7 @@ namespace KafkaProducer
             }
         }
 
-        public static async void Main()
+        static async Task Main(string[] args)
         {
             Console.WriteLine("Hello Event Hub Kafka Client");
 
@@ -67,8 +59,7 @@ namespace KafkaProducer
 
             IProducer<Null, string> producer = producerBuilder.SetOAuthBearerTokenRefreshHandler(OauthCallback).Build();
 
-
-            for (int x = 0; x < 100; x++)
+            for (int x = 0; x < 10; x++)
             {
                 var msg = new Message<Null, string> { Value = string.Format("This is a sample message - msg # {0} at {1}", x, DateTime.Now.ToString("yyyMMdd_HHmmSSfff")) };
 
@@ -77,9 +68,7 @@ namespace KafkaProducer
 
                 Console.WriteLine($"Message {result.Value} sent to partition {result.TopicPartition} with result {result.Status}");
             }
-
             Console.WriteLine("Producer complete");
-
         }
 
     }
